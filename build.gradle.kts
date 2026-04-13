9bfec918ab0819dc3eeca69d9da27599703d18d9
RealMG <realmg51@gmail.com>
2026-04-13 20:16:04 +0700

    Update Android and Kotlin plugin versions
    Updated plugin versions for Android and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
